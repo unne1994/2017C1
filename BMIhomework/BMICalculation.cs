2d685d78424a9ba8e6da7a85a6bb6d4f7148aa34
unknown <>
2017-02-18 19:55:41 +0800

    homework

---
+++ BMIhomework/BMICalculation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMIhomework
+{
+    class BMICalculation
+    {
+        private double _x; //身高
+        private double _y; //體重
+
+        public double x
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        public double y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+        public double add()
+        {
+         return _y/(_x*_x);
+
+        }
+
+    }
+}
---
+++ BMIhomework/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMIhomework
+{
+    public partial class Form1 : Form
+    {
+        private BMICalculation _data;
+        public Form1()
+        {
+            
+            InitializeComponent();
+            _data = new BMICalculation(); 
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _data.x = double.Parse(textBox1.Text);
+            _data.y = double.Parse(textBox2.Text);
+
+            double z = _data.add();
+
+            if (z < 18.5)
+            {
+               MessageBox.Show("BMI:"+_data.add().ToString("#0.0") + "\n" + "評語:過瘦");
+            }
+            if (z >= 18.5 && z < 24)
+            {
+                MessageBox.Show("BMI:" + _data.add().ToString("#0.0") + "\n" + "評語:正常範圍");
+              
+            }
+            if (z >= 24 && z < 27)
+            {
+                MessageBox.Show("BMI:" + _data.add().ToString("#0.0") + "\n" + "評語:過重");
+           
+            }
+            if (z >= 27 && z < 30)
+            {
+                MessageBox.Show("BMI:" + _data.add().ToString("#0.0") + "\n" + "評語:輕度肥胖");
+          
+            }
+            if (z >= 30 && z < 35)
+            {
+                MessageBox.Show("BMI:" + _data.add().ToString("#0.0") + "\n"+ "評語:中度肥胖");
+              
+            }
+            if (z > 35)
+            {
+                MessageBox.Show("BMI:" + _data.add().ToString("#0.0") +"\n"+"重度肥胖");
+                
+            }
+
+        }
+
+    }
+}
